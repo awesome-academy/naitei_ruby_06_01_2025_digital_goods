4b8e7a6d2eb6b0b5cef039fdebd1a55ae3bc5352
khaitd-3535 <tran.duc.khai@sun-asterisk.com>
2025-02-25 09:13:33 +0700

    view product by category role admin

---
+++ app/javascript/custom/filter_category.js
@@ -0,0 +1,43 @@
+﻿document.addEventListener('turbo:load', initializeFilterContainers);
+document.addEventListener('turbo:render', () => setTimeout(initializeFilterContainers, 50));
+
+function initializeFilterContainers() {
+  const filterContainers = document.querySelectorAll('.filter-container');
+
+  filterContainers.forEach(container => {
+    const listCategories = container.querySelector('.list-categories');
+
+    if (!listCategories) return;
+
+    container.removeEventListener('click', toggleFilter);
+    container.addEventListener('click', toggleFilter);
+  });
+
+  document.removeEventListener('click', closeDropdown);
+  document.addEventListener('click', closeDropdown);
+}
+
+function toggleFilter(event) {
+  event.stopPropagation();
+  const container = event.currentTarget;
+  const listCategories = container.querySelector('.list-categories');
+
+  if (listCategories) {
+    listCategories.classList.toggle('active');
+  }
+}
+
+function closeDropdown(event) {
+  document.querySelectorAll('.filter-container').forEach(container => {
+    if (!container.contains(event.target)) {
+      const listCategories = container.querySelector('.list-categories');
+      if (listCategories) listCategories.classList.remove('active');
+    }
+  });
+}
+
+const observer = new MutationObserver(() => {
+  initializeFilterContainers();
+});
+
+observer.observe(document.body, { childList: true, subtree: true });
--- app/models/category.rb
+++ app/models/category.rb
@@ -3,6 +3,13 @@ class Category < ApplicationRecord
   has_many :products, through: :product_categories, source: :product
 
   scope :sub_categories_of, lambda {|category_path|
-    where("path LIKE ?", "#{category_path}/%")
+    where("path = ? OR path LIKE ?", category_path, "#{category_path}/%")
   }
+
+  scope :with_level, lambda {|level, path = nil|
+    where(level:)
+      .where("path LIKE ? OR path IS NULL", "#{path}/%")
+  }
+
+  scope :find_category_name, ->(path){where(path:).pick(:name)}
 end
